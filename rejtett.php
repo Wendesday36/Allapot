bc1fa71c234638c025a5c19dd7949080bd5e8bc8
Wendesday36 <bertazoeanna@gmail.com>
2023-04-26 10:38:34 +0200

    rendezes

--- rejtett.php
+++ rejtett.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-<?php
-    $szamlalo= 0; 
-    if (isset($_POST["szamlalo"])) { 
-        $szamlalo = $_POST["szamlalo"]; 
+    <?php
+    $szamlalo = 0;
+    if (isset($_POST["szamlalo"])) {
+        $szamlalo = $_POST["szamlalo"];
     }
-    echo $szamlalo."<br>"; 
+    echo $szamlalo . "<br>";
     $szamlalo++;
-?>
-    <form action-"rejtett.php" method="post"> <input type="hidden" name="szamlalo"
-    value="<?php echo $szamlalo?>"> <input type = "submit" value="Növel"> 
-</form>
+    ?>
+    <form action-"rejtett.php" method="post"> <input type="hidden" name="szamlalo" value="<?php echo $szamlalo ?>"> <input type="submit" value="Növel">
+    </form>
 
 </body>
+
 </html>
